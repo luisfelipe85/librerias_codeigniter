24181d10e44821c7635a1648c152c42f911cd5fa
luisfelipe85 <71136024+luisfelipe85@users.noreply.github.com>
2021-02-16 14:17:51 -0600

    Template index
    Template index

---
+++ template_index.php
@@ -0,0 +1,227 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+    <title>Ingreso al Sistema</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/logo/favicon.ico">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/bootstrap/offcanvas.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/responsive/css/responsive.bootstrap4.min.css">
+
+    <!-- CSS Global Compulsory -->
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/css/unify-core.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/css/unify-components.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/css/unify-globals.css">
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/css/custom.css">-->
+    <link rel="stylesheet" href="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/icon-awesome/css/font-awesome.min.css">
+    <!-- JS -->
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.js"></script>-->
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/bootstrap/bootstrap.min.js"></script>
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/js/dataTables.bootstrap4.min.js"></script>
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/responsive/js/dataTables.responsive.min.js"></script>
+    <script src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/vendor/datatables/responsive/js/responsive.bootstrap4.min.js"></script>
+
+
+  </head>
+
+  <body>
+  <!--<body class="container u-shadow-v1-7 g-pa-0">-->
+  <header id="js-header" class="u-header u-header--sticky-top u-header--toggle-section" data-header-fix-moment="300">
+    <div class="u-header__section u-header__section--light  g-transition-0_3" data-header-fix-moment-exclude="" data-header-fix-moment-classes="u-shadow-v18 g-py-0" style="background-color: #333">
+      <nav class="navbar navbar-expand-lg g-pa-0 g-pt-4 g-pb-4">
+        <div class="container">
+          <button class="navbar-toggler navbar-toggler-right btn g-line-height-1 g-brd-none g-pa-0 g-pos-abs g-top-3 g-right-0" type="button" aria-label="Toggle navigation" aria-expanded="false" aria-controls="navBar" data-toggle="collapse" data-target="#navBar">
+            <span class="hamburger hamburger--slider">
+              <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+              </span>
+            </span>
+          </button>
+
+          <a href="https://chiapas.gob.mx/" class="navbar-brand"><img src="https://icti.chiapas.gob.mx/assets/logo/escudo-icono.png" alt=""></a>
+          <a href="https://chiapas.gob.mx/" class="g-color-white-opacity-0_9 g-font-size-16 g-font-weight-300 g-font-secondary">chiapas<span class="g-color-white-opacity-0_6">.gob.mx</span></a>
+
+          <div class="collapse navbar-collapse align-items-center flex-sm-row g-pt-10 g-pt-5--lg" id="navBar">
+            <ul class="navbar-nav ml-auto g-font-size-16 g-font-weight-100">
+              <li class="nav-item g-mx-10--lg"><a href="https://chiapas.gob.mx/tramites" class="nav-link g-color-white-opacity-0_9 g-text-underline--hover">Trámites</a></li>
+              <li class="nav-item g-mx-10--lg"><a href="https://chiapas.gob.mx/gobierno" class="nav-link g-color-white-opacity-0_9 g-text-underline--hover">Gobierno</a></li>
+              <li class="nav-item g-mx-10--lg"><a href="https://chiapas.gob.mx/participa" class="nav-link g-color-white-opacity-0_9 g-text-underline--hover">Participa</a></li>
+              <li class="nav-item g-mx-10--lg"><a href="http://gubernatura.transparencia.chiapas.gob.mx" class="nav-link g-color-white-opacity-0_9 g-text-underline--hover">Transparencia</a></li>
+              <li class="nav-item g-mx-10--lg"><a href="https://chiapas.gob.mx/busquedas" class="nav-link g-color-white-opacity-0_9 g-text-underline--hover"><i class="fa fa-search"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <section>
+      <div class="g-brd-around  g-brd-1 g-brd-gray-light-v4-top g-brd-gray-light-v4-bottom g-color-white-opacity-0_9" role="alert" style="background-color: #F8F9FA;">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light g-pa-0 g-right-0 g-pt-4 g-pb-4" style="background-color: #F8F9FA">
+            <a class="navbar-brand g-color-morado g-font-weight-600 g-font-size-16  d-none d-sm-none d-md-none d-lg-none d-xl-block g-text-underline--hover" href="http://educacionchiapas.gob.mx/" target="_blank">Secretaria de Educaci&oacute;n</a>
+            <a class="navbar-brand g-color-morado g-font-weight-600 g-font-size-16  d-sm-block d-md-block d-lg-block d-xl-none g-text-underline--hover" href="http://educacionchiapas.gob.mx/">SE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto g-font-size-15 g-font-weight-100">
+                <li class="nav-item g-mx-5--lg"><a class="nav-link g-color-white--hover g-bg-morado--hover g-rounded-3 g-color-black-opacity-0_9" href="javascript:void(0)"></a></li>
+                <li class="nav-item g-mx-5--lg"><a class="nav-link g-color-white--hover g-bg-morado--hover g-rounded-3 g-color-black-opacity-0_9" href="http://sistemasse.educacionchiapas.gob.mx/financieros/login/logout">Cerrar Session</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </section>
+
+  </header>
+
+
+  <section class="g-py-130 container">
+
+  <div class="container-fluid">
+
+
+  <div class="card mb-4">
+    <div class="card-header">
+      <i class="fa fa-lock mr-1"></i>Acceso al Sistema    </div><!-- /.card-header -->
+      <div class="card-body">
+        <div class="row">
+
+            <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Acceso al Sistema</h3>
+              </div>
+              <div class="panel-body">
+                <form action="http://sistemasse.educacionchiapas.gob.mx/financieros/login/access" autocomplete="off" method="post" accept-charset="utf-8">
+                  <fieldset>
+                    <div class="form-group">
+                      <input type="text" name="usuario" value="" class="form-control" placeholder="Usuario" autofocus="autofocus"  />
+                                        </div>
+                    <div class="form-group">
+                      <input type="password" name="password" value="" class="form-control" placeholder="Password"  />
+                                          </div>
+
+                    <!-- Change this to a button or input when using this as a form -->
+                    <!--<a href="index.php" class="btn btn-lg btn-success btn-block">Login</a>-->
+                    <input type="submit" name="mysubmit" value="Ingresar"  class="btn btn-lg btn-success btn-block" />
+                  </fieldset>
+                  </form>              </div>
+            </div>
+          </div>
+
+        </div><!-- /.row -->
+      </div><!-- /.card-body -->
+  </div><!-- /.card mb-4 -->
+</div><!-- /.container-fluid -->
+
+  </section>
+
+    <div class="g-bg-black-opacity-0_8 g-color-white-opacity-0_8 g-py-20" style="background-color: #c2b082;">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2 col-md-6 g-mb-20 g-mb-0--lg">
+            <img class="img-fluid" src="http://sistemasse.educacionchiapas.gob.mx/financieros/assets/logo/logo-blanco.png" alt="Logo">
+          </div>
+
+          <div class="col-lg-3 col-md-6 g-mb-20 g-mb-0--lg">
+            <!--<div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8"><h2 class="u-heading-v2__title h6 mb-0">Acerca del Instituto</h2></div>
+            <ul class="list-unstyled">
+              <li class="g-mb-5"><i class="fa fa-angle-right g-mr-5"></i><a class="g-color-white-opacity-0_8" href="https://icti.chiapas.gob.mx/conocenos">Conócenos</a></li>
+              <li class="g-mb-5"><i class="fa fa-angle-right g-mr-5"></i><a class="g-color-white-opacity-0_8" href="http://www.chiapas.gob.mx/funcionarios/estatal/ejecutivo/icti">Directorio de Funcionarios</a></li>
+            </ul>-->
+          </div>
+
+          <div class="col-lg-3 col-md-6 g-mb-20 g-mb-0--lg">
+            <!--<div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8"><h2 class="u-heading-v2__title h6 mb-0">Espacios de divulgación</h2></div>
+            <ul class="list-unstyled">
+              <li class="g-mb-5"><i class="fa fa-angle-right g-mr-5"></i><a class="g-color-white-opacity-0_8" href="http://icti.chiapas.gob.mx/espacios/museo/">Museo Chiapas de Ciencia</a></li>
+              <li class="g-mb-5"><i class="fa fa-angle-right g-mr-5"></i><a class="g-color-white-opacity-0_8" href="http://icti.chiapas.gob.mx/espacios/planetario/">Planetario Tuxtla</a></li>
+              <li class="g-mb-5"><i class="fa fa-angle-right g-mr-5"></i><a class="g-color-white-opacity-0_8" href="http://icti.chiapas.gob.mx/espacios/la-oruga/">La Oruga de la Ciencia</a></li>
+            </ul>-->
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8"><h2 class="u-heading-v2__title h6 mb-0">Contáctanos</h2></div>
+            <address class="g-bg-no-repeat g-line-height-2 g-mt-minus-4" style="background-image: url(http://sistemasse.educacionchiapas.gob.mx/financieros/assets/logo/map2.png);">
+              Unidad Administrativa, Edificio B; Col. Maya<br>Tuxtla Gutiérrez, Chiapas<br>Conmutador: Conmutador: 961 61 883 00
+            </address>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="g-bg-black-opacity-0_9 g-color-white-opacity-0_8 g-py-20">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-8 text-center text-md-left g-mb-10 g-mb-0--md">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="d-lg-flex">
+                  <small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md g-font-weight-300">Trámites por Internet:</small>
+                  <ul class="u-list-inline">
+                    <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/servicios-por-internet/#nav-1-1-vehicular">Vehicular</a></li>
+                    <li class="list-inline-item"><span>|</span></li>
+                    <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/servicios-por-internet/#nav-1-1-inmueble">Inmuebles</a></li>
+                    <li class="list-inline-item"><span>|</span></li>
+                    <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/servicios-por-internet/#nav-1-1-personal">Personales</a></li>
+                    <li class="list-inline-item"><span>|</span></li>
+                    <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/servicios-por-internet/#nav-1-1-empresarial">Empresas</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="d-lg-flex">
+                  <small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md">Mantente informado:</small>
+                  <ul class="u-list-inline">
+                  <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/tv/">TV</a></li>
+                  <li class="list-inline-item"><span>|</span></li>
+                  <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/radio/">Radio</a></li>
+                  <li class="list-inline-item"><span>|</span></li>
+                  <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/prensa/">Prensa</a></li>
+                  <li class="list-inline-item"><span>|</span></li>
+                  <li class="list-inline-item"><a class="g-color-white-opacity-0_8 g-color-white--hover" href="http://www.chiapas.gob.mx/redes/">Redes sociales</a></li>
+                  </ul>               </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 align-self-center">
+          </div>
+
+          <div class="col-md-0 align-self-center">
+            <ul class="list-inline text-center text-md-right mb-0">
+              <li class="list-inline-item g-mr-10"><a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-bg-white-opacity-0_2--hover g-color-white-opacity-0_6" href="https://www.facebook.com/SEChiapas/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+              <li class="list-inline-item g-mr-10"><a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-bg-white-opacity-0_2--hover g-color-white-opacity-0_6" href="https://www.twitter.com/Edu_Chis/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+               <li class="list-inline-item g-mr-10"><a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-bg-white-opacity-0_2--hover g-color-white-opacity-0_6" href="https://www.youtube.com/channel/UC_fnB2CvU2WVBPETKGO5mgQ" target="_blank"><i class="fa fa-youtube"></i></a></li>
+              <li class="list-inline-item g-mr-10"><a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-0_1 g-bg-white-opacity-0_2--hover g-color-white-opacity-0_6" href="https://www.instagram.com/sechiapas/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </main>
+
+
+  </body>
+</html>
+
